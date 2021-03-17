fa161951cbfcf6a36aaa05c98696e701a20c019d
OlehTovkaniuk <tovkaniuk@gmail.com>
2021-03-17 12:22:20 +0200

    created Map component

---
+++ src/components/map/Map.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+import './Map.css';
+
+export default function Map({ ipData, isFetching, isError }) {
+
+    return (
+        <div id="map-container">
+            ----Map----
+        </div>
+    )
+}
